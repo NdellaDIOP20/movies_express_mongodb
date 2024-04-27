c3b573f3d1b8b744bfb9a7a839a9424ea8e465d7
NdellaDIOP20 <168141422+NdellaDIOP20@users.noreply.github.com>
2024-04-27 20:45:04 +0000

    Add files via upload

---
+++ models/movies.model.js
@@ -0,0 +1,16 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema({
+        tire: String,
+        synopsis: String,
+        release: Boolean,
+    }, { timestamps: true });
+
+    schema.method("toJSON", function() {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Movie = mongoose.model("movie", schema);
+    return Movie;
+};
